b4a2531a67f78acb3891f88918778f2c0082ebd5
jonyCodeur <jonathanmbepongo12@gmail.com>
2026-03-03 18:38:25 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/congo_chalenge/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.congo_chalenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/splash_view.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'mywebview_page';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5),()=>Get.to(()=>MyWebView()));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.maxFinite,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            const Color.fromARGB(255, 8, 120, 211),
+            const Color.fromARGB(255, 1, 25, 61),          
+          ],
+          tileMode: TileMode.mirror,
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height:90,
+              width: 90,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              color: Colors.blue.withValues(alpha: 0.8),
+              ),
+              child: Icon(Icons.library_music_rounded,color: Colors.red.shade600,size: 50,)),
+            SizedBox(height: 10),
+            Center(
+              child: Text("Congo\nchalenge",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
+              ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
