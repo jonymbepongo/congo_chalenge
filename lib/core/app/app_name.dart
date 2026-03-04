1fb37425e6daa2fac5883a31ed74631765080e8e
jonyCodeur <jonathanmbepongo12@gmail.com>
2026-03-04 13:44:26 +0100

    première lancement stable

---
+++ lib/core/app/app_name.dart
@@ -0,0 +1,17 @@
+abstract class AppName{
+
+  static const String appName = 'Congo chalenge';
+  // static const String logoPath = 'assets/logo_napiland.png';
+  static const String ipAdresse = '192.168.0.194';
+  static const String url = "http://$ipAdresse:8080";
+  
+  static const String leafLoading='assets/lottie/leafloading.gif';
+
+  /// Slogan ou description courte (utilisé par exemple dans la SplashView)
+  static const String tagline = 'Votre routine capillaire personalisée,simple et efficace';
+  static const String slogan='Rejoignez la communauté $appName pour prendre soin de vos cheveux afro';
+  
+  static const String errorMessagehairAfro="pas des cheveux afro detectés";
+  static const String errorMessageBlurPic="image floue";
+
+}
