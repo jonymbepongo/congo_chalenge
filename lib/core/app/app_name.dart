8e3ca0d9922997dc947ec5dcd1e9b3621466eac5
jonyCodeur <jonathanmbepongo12@gmail.com>
2026-03-06 19:15:37 +0100

    backup stable

--- lib/core/app/app_name.dart
+++ lib/core/app/app_name.dart
@@ -5,7 +5,7 @@ abstract class AppName{
   static const String ipAdresse = '192.168.0.194';
   static const String url = "http://$ipAdresse:8080";
   
-  static const String leafLoading='assets/lottie/leafloading.gif';
+  // static const String leafLoading='assets/lottie/leafloading.gif';
 
   /// Slogan ou description courte (utilisé par exemple dans la SplashView)
   static const String tagline = 'Votre routine capillaire personalisée,simple et efficace';
